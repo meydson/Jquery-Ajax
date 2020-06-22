723c6822a34070587ed24801f6428cdc77f9c18a
meydson <64757536+meydson@users.noreply.github.com>
2020-06-21 21:49:34 -0300

    configuração inicial

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const { type, get } = require("jquery")
+
+function consultaCep() {
+    let cep = document.getElementById("cep").value
+    let url = "https://viacep.com.br/ws/" + cep + "/json/"
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response) {
+            console.log(response)
+            document.getElementById("logradouro").innerHTML = response.logradouro
+            $("#bairro").html(response.bairro)
+        }
+    })
+}
