e6b477d31fe1c54a6f6cd797c7fe816994c0a71e
meydson <64757536+meydson@users.noreply.github.com>
2020-06-21 22:46:26 -0300

    adicionada tablea com o resultado e efeito para aparecer e desaparecer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //const { type, get } = require("jquery")
 
 function consultaCep() {
+    $(".barra-progresso").show()
     let cep = document.getElementById("cep").value
     let url = "https://viacep.com.br/ws/" + cep + "/json/"
     $.ajax({
@@ -10,6 +11,18 @@ function consultaCep() {
             console.log(response)
             //document.getElementById("logradouro").innerHTML = response.logradouro
             //$("#bairro").html(response.bairro)
+            $("#logradouro").html(response.logradouro)
+            $("#bairro").html(response.bairro)
+            $("#localidade").html(response.localidade)
+            $("#uf").html(response.uf)
+            $("#titulo_cep").html("CEP: " + response.cep)
+            $(".cep").show()
+            $(".barra-progresso").hide()
         }
     })
 }
+
+$(() => {
+    $(".cep").hide()
+    $(".barra-progresso").hide()
+})
